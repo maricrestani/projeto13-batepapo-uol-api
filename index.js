06dc8e581eca444455029b19ebf4e06d6265ebcc
mcrestani <marianascrestani@gmail.com>
2022-11-12 17:08:07 -0300

    feat: post do usuário no diretório /participants

--- index.js
+++ index.js
@@ -1,22 +1,49 @@
 import express from "express";
 import dotenv from "dotenv";
 import { MongoClient } from "mongodb";
-import core from "cors";
+import cors from "cors";
+import dayjs from "dayjs";
+import joi from "joi";
 
 const app = express();
-const mongoClient = new MongoClient(process.env.MONGO_URI);
+
 dotenv.config();
 app.use(express.json());
 app.use(cors());
 
-let db = mongoClient.db("uol");
+const mongoClient = new MongoClient(process.env.MONGO_URI);
+let db;
 
-try {
-  await mongoClient.conect();
-  console.log("MongoDB connected");
-} catch (err) {
-  console.log(err);
-}
+mongoClient
+  .connect()
+  .then(() => {
+    console.log("MongoDB connected");
+    db = mongoClient.db("uol");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.post("/participants", async (req, res) => {
+  const { name } = req.body;
+
+  if (!name) {
+    res.status(422).send({ error: "name deve ser strings não vazio" });
+    return;
+  }
+
+  const loginMessage = {
+    from: name,
+    to: "Todos",
+    text: "entra na sala...",
+    type: "status",
+    time: dayjs(participants.lastStatus).format("HH:mm:ss"),
+  };
+
+  await db.collection("users").insertOne({ name, lastStatus: Date.now() });
+  await db.collection("messages").insertOne(loginMessage);
+  res.sendStatus(201);
+});
 
 app.listen(process.env.PORT, () => {
   console.log(`Server runnin in port: ${process.env.PORT}`);
